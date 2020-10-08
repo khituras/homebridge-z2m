14b4c633d51174c8163c59bd90083aa4aa3bd62b
khituras <chew@gmx.net>
2020-10-08 15:26:56 +0200

    More messages.

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -195,7 +195,7 @@ export class Zigbee2mqttAccessory {
   private createServiceForKey(key: string, state: Map<string, CharacteristicValue> | undefined = undefined,
     handledKeys: Set<string> | undefined = undefined) {
     // Create new service (if possible)
-    this.log.info("Erik: got key " + key);
+    this.log.info("Erik: got key " + key + ", state: " + state + " and handled keys: " + handledKeys);
     switch (key) {
       case 'humidity':
       {
